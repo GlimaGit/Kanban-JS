b806e017996d05c160f8622dad8c34508953e9bf
Lucas Frois <lucas.frois@lambda3.com.br>
2021-08-16 21:32:56 -0300

    adiciona javascript index

--- dist/index.174b228b.js
+++ dist/index.174b228b.js
@@ -455,6 +455,23 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}]},["97JVM"], null, "parcelRequire67cb")
-alert("oi!");
+const githubUserRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+document.getElementById("login").addEventListener("click", function(event) {
+    event.preventDefault();
+    // 1. Validar usuário inserido
+    // 2. Chamar API do Github e obter dados
+    // 3. Salvar dados no localStorage
+    // 4. Redirecionar para a próxima página /boards
+    const user = document.getElementById("input-login").value;
+    if (user || githubUserRegex.test(user)) return;
+    console.log(user);
+    fetch(`https://api.github.com/users/${user}}`).then((res)=>res.json()
+    ).then((data)=>{
+        const stringified = JSON.stringify(data);
+        alert(`${stringified}`);
+        localStorage.setItem(data.id, stringified);
+    }).catch(()=>alert("erro ao buscar usuário no github")
+    );
+});
 
 //# sourceMappingURL=index.174b228b.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-alert("oi!");
+const githubUserRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+
+document.getElementById("login").addEventListener("click", function (event) {
+  event.preventDefault();
+  // 1. Validar usuário inserido
+  // 2. Chamar API do Github e obter dados
+  // 3. Salvar dados no localStorage
+  // 4. Redirecionar para a próxima página /boards
+
+  const user = document.getElementById("input-login").value;
+
+  if (user || githubUserRegex.test(user)) {
+    return;
+  }
+  console.log(user);
+
+  fetch(`https://api.github.com/users/${user}}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const stringified = JSON.stringify(data);
+      alert(`${stringified}`);
+      localStorage.setItem(data.id, stringified);
+    })
+    .catch(() => alert("erro ao buscar usuário no github"));
+});
